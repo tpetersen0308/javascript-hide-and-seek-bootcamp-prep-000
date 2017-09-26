0c6e8ece3fbe31673c24ba408564e0c058a0c3a4
Travis Petersen <travis.e.petersen@gmail.com>
2017-09-26 22:48:08 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,23 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector(".target")
+}
+
+function increaseRankBy(n) {
+  var lis = document.getElementById("app").querySelectorAll(".ranked-list")
+
+  for(var i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML + n, 10)
+  }
+}
+
+function deepestChild() {
+  var current = document.getElementById("grand-node").querySelector("div")
+  while(current.querySelector("div")) {
+    current = current.querySelector("div")
+  }
+  return current
+}
